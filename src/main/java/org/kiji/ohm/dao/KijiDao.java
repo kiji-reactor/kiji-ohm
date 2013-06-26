b3b06f3ff84835b8aac77cc034978648fb6f99a4
Christophe Taton <taton@wibidata.com>
2013-06-26 13:23:09 -0700

    Introduce EntitySpec

--- src/main/java/org/kiji/ohm/dao/KijiDao.java
+++ src/main/java/org/kiji/ohm/dao/KijiDao.java
@@ -4,11 +4,13 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Iterator;
-import java.util.Map.Entry;
-import java.util.NavigableMap;
+import java.util.List;
+import java.util.Map;
 
 import com.google.common.base.Preconditions;
-
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang.NotImplementedException;
 import org.apache.hadoop.hbase.HConstants;
 import org.slf4j.Logger;
@@ -19,7 +21,6 @@ import org.kiji.ohm.annotations.KijiColumn;
 import org.kiji.ohm.annotations.KijiEntity;
 import org.kiji.schema.EntityId;
 import org.kiji.schema.Kiji;
-import org.kiji.schema.KijiCell;
 import org.kiji.schema.KijiDataRequest;
 import org.kiji.schema.KijiDataRequestBuilder;
 import org.kiji.schema.KijiDataRequestBuilder.ColumnsDef;
@@ -33,11 +34,14 @@ import org.kiji.schema.avro.RowKeyFormat2;
 /**
  * Kiji Data Access Object (DAO).
  */
-public class KijiDao implements Closeable {
+public final class KijiDao implements Closeable {
   private static final Logger LOG = LoggerFactory.getLogger(KijiDao.class);
 
   /** Kiji instance. */
   private final Kiji mKiji;
+
+  /** Map of entity specifications. */
+  private final Map<Class<?>, EntitySpec<?>> mEntitySpec = Maps.newHashMap();
 
   /**
    * Initializes a new instance of Kiji Data Access Object.
@@ -55,6 +59,23 @@ public class KijiDao implements Closeable {
   public void close() throws IOException {
     mKiji.release();
   }
+
+  // -----------------------------------------------------------------------------------------------
+
+  private <T> EntitySpec<T> getEntitySpec(Class<T> klass) {
+    synchronized(mEntitySpec) {
+      @SuppressWarnings("unchecked")
+      final EntitySpec<T> spec = (EntitySpec<T>) mEntitySpec.get(klass);
+      if (spec != null) {
+        return spec;
+      }
+      final EntitySpec<T> newSpec = new EntitySpec<T>(klass);
+      mEntitySpec.put(klass, newSpec);
+      return newSpec;
+    }
+  }
+
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * Shortcut for {@link #select(Class, EntityId, long, long).
@@ -94,11 +115,10 @@ public class KijiDao implements Closeable {
   public <T> T select(Class<T> klass, EntityId entityId, long startTime, long endTime)
       throws IOException {
 
-    final KijiEntity entity = klass.getAnnotation(KijiEntity.class);
-    Preconditions.checkArgument(entity != null, "Class '{}' has no @KijiEntity annotation.", klass);
+    final EntitySpec<T> spec = getEntitySpec(klass);
 
     // TODO: Use a pool of tables and/or table readers
-    final KijiTable table = mKiji.openTable(entity.table());
+    final KijiTable table = mKiji.openTable(spec.getTableName());
     try {
       // TODO: Support RowKeyFormat?
       final RowKeyFormat2 rowKeyFormat =
@@ -108,7 +128,7 @@ public class KijiDao implements Closeable {
       try {
         final KijiDataRequestBuilder builder = KijiDataRequest.builder();
         builder.withTimeRange(startTime, endTime);
-        parseColumnRequests(klass, builder, rowKeyFormat);
+        spec.populateColumnRequests(builder);
         final KijiDataRequest dataRequest = builder.build();
 
         final KijiRowData row = reader.get(entityId, dataRequest);
@@ -146,63 +166,6 @@ public class KijiDao implements Closeable {
   }
 
   /**
-   * Extracts column data requests from an annotated entity class.
-   *
-   * @param klass
-   * @return
-   */
-  private static void parseColumnRequests(
-      Class<?> klass,
-      KijiDataRequestBuilder builder,
-      RowKeyFormat2 rowKeyFormat) {
-
-    LOG.debug("Parsing column requests from entity '{}'.", klass);
-
-    for (final Field field : klass.getDeclaredFields()) {
-      final KijiColumn column = field.getAnnotation(KijiColumn.class);
-      final EntityIdField eidField = field.getAnnotation(EntityIdField.class);
-
-      if ((column != null) && (eidField != null)) {
-        throw new IllegalArgumentException(String.format(
-            "Field '%s' cannot have both @KijiColumn and @EntityIdField annotations.", field));
-
-      } else if (column != null) {
-
-        if (column.qualifier().isEmpty()) {
-          // TODO: Must be a map-type family!
-          // TODO: Field type must be a map of qualifier -> values/timeseries.
-          LOG.debug("Requesting family '{}' for field '{}'.", column.family(), field);
-          builder.addColumns(ColumnsDef.create()
-              .withMaxVersions(column.maxVersions())
-              .addFamily(column.family()));
-        } else {
-          LOG.debug("Requesting column '{}:{}' for field '{}'.",
-              column.family(), column.qualifier(), field);
-          builder.addColumns(ColumnsDef.create()
-              .withMaxVersions(column.maxVersions())
-              .add(column.family(), column.qualifier()));
-        }
-
-      } else if (eidField != null) {
-        LOG.debug("Validating entity ID field '{}' with component name '{}'.",
-            field, eidField.component());
-        boolean found = false;
-        for (final RowKeyComponent rkc : rowKeyFormat.getComponents()) {
-          if (rkc.getName().equals(eidField.component())) {
-            found = true;
-            break;
-          }
-        }
-        Preconditions.checkArgument(found, "Field '{}' maps to unknown entity ID component '{}'.",
-            field, eidField.component());
-
-      } else {
-        LOG.debug("Ignoring field '{}' with no annotation.", field);
-      }
-    }
-  }
-
-  /**
    * Creates an entity object from a KijiRowData.
    *
    * @param klass Class of the entity object to instantiate and populate.
@@ -229,42 +192,15 @@ public class KijiDao implements Closeable {
       } else if (column != null) {
         if (column.qualifier().isEmpty()) {
           LOG.debug("Populating field '{}' from map-type family '{}'.", field, column.family());
-          MapTypeValue<Object> mapValues = new MapTypeValue<Object>();
           if (column.maxVersions() == 1) {
             // Field is a map: qualifier -> single value:
-            // TODO: Let's find a way to decorate the underlying NavigableMap instead of iterating
-            // over it to construct this MapTypeValue.
-            NavigableMap<String, NavigableMap<Long, KijiCell<Object>>> cells = row.getCells(column
-                .family());
-            for (Entry<String, NavigableMap<Long, KijiCell<Object>>> e : cells.entrySet()) {
-              String qualifier = e.getKey();
-              NavigableMap<Long, KijiCell<Object>> tCells = e.getValue();
-              for (Entry<Long, KijiCell<Object>> ee : tCells.entrySet()) {
-                mapValues.put(qualifier, new TCell<Object>(ee.getKey(), ee.getValue().getData()));
-              }
-            }
-            LOG.debug("Populating single version map field '{}'.", field);
+            throw new NotImplementedException();
+          } else {
+            // Field is a map: qualifier -> time-series:
+            throw new NotImplementedException();
           }
-          else //Multi-version map type family
-          {
-              // TODO: Let's find a way to decorate the underlying NavigableMap instead of iterating
-              // over it to construct this MapTypeValue.
-              NavigableMap<String, NavigableMap<Long, KijiCell<Object>>> cells = row.getCells(column
-                  .family());
-              for (Entry<String, NavigableMap<Long, KijiCell<Object>>> e : cells.entrySet()) {
-                String qualifier = e.getKey();
-                NavigableMap<Long, KijiCell<Object>> tCells = e.getValue();
-                TimeSeries<Object> timeseries = new TimeSeries<Object>();
-                for (Entry<Long, KijiCell<Object>> ee : tCells.entrySet()) {
-                  timeseries.put(ee.getKey(), new TCell<Object>(ee.getKey(), ee.getValue().getData()));
-                }
-                mapValues.put(qualifier, timeseries);
-              }
-              LOG.debug("Populating single version map field '{}'.", field);
-              field.set(entity, mapValues);
-          }
-          field.set(entity, mapValues);
-        } else { //multi-version group type family.
+
+        } else {
           if (column.maxVersions() == 1) {
             // Field represents a single value from a fully-qualified column:
             LOG.debug("Populating field '{}' from column '{}:{}'.",
@@ -272,7 +208,9 @@ public class KijiDao implements Closeable {
             Object value = row.getMostRecentValue(column.family(), column.qualifier());
             if (field.getType() == String.class) {
               // Automatically converts CharSequence to java String if necessary:
-              value = value.toString();
+              if (value != null) {
+                value = value.toString();
+              }
             }
             field.setAccessible(true);
             field.set(entity, value);
@@ -314,4 +252,109 @@ public class KijiDao implements Closeable {
       throw new RuntimeException(ie);
     }
   }
+
+  // -----------------------------------------------------------------------------------------------
+
+  /**
+   * Specification of an annotated entity class.
+   */
+  private static final class EntitySpec<T> {
+    private final Class<T> mClass;
+
+    private final String mTableName;
+
+    /** Fields populated from a Kiji row (from either a column or the entity ID). */
+    private final ImmutableList<Field> mFields;
+
+    /** Fields populated from the row columns. */
+    private final ImmutableList<Field> mColumnFields;
+
+    /** Fields populated from the row entity ID components. */
+    private final ImmutableList<Field> mEntityIdFields;
+
+    public EntitySpec(Class<T> klass) {
+      mClass = klass;
+
+      final KijiEntity entity = klass.getAnnotation(KijiEntity.class);
+      Preconditions.checkArgument(entity != null,
+          "Class '{}' has no @KijiEntity annotation.", klass);
+      mTableName = entity.table();
+
+      final List<Field> fields = Lists.newArrayList();
+      final List<Field> columnFields = Lists.newArrayList();
+      final List<Field> entityIdFields = Lists.newArrayList();
+
+      for (final Field field : mClass.getDeclaredFields()) {
+        final KijiColumn column = field.getAnnotation(KijiColumn.class);
+        final EntityIdField eidField = field.getAnnotation(EntityIdField.class);
+
+        if ((column != null) && (eidField != null)) {
+          throw new IllegalArgumentException(String.format(
+              "Field '%s' cannot have both @KijiColumn and @EntityIdField annotations.", field));
+
+        } else if (column != null) {
+          LOG.debug("Validating column field '{}'.", field);
+          field.setAccessible(true);
+          fields.add(field);
+          columnFields.add(field);
+
+        } else if (eidField != null) {
+          LOG.debug("Validating entity ID field '{}'.", field);
+          field.setAccessible(true);
+          fields.add(field);
+          entityIdFields.add(field);
+
+//          // Validate the entity ID specification:
+//          boolean found = false;
+//          for (final RowKeyComponent rkc : rowKeyFormat.getComponents()) {
+//            if (rkc.getName().equals(eidField.component())) {
+//              found = true;
+//              break;
+//            }
+//          }
+//          Preconditions.checkArgument(found, "Field '{}' maps to unknown entity ID component '{}'.",
+//              field, eidField.component());
+
+        } else {
+          LOG.debug("Ignoring field '{}' with no annotation.", field);
+        }
+      }
+
+      mFields = ImmutableList.copyOf(fields);
+      mColumnFields = ImmutableList.copyOf(columnFields);
+      mEntityIdFields = ImmutableList.copyOf(entityIdFields);
+    }
+
+    public Class<T> getEntityClass() {
+      return mClass;
+    }
+
+    public String getTableName() {
+      return mTableName;
+    }
+
+    public void populateColumnRequests(KijiDataRequestBuilder builder) {
+      for (final Field field : mColumnFields) {
+        final KijiColumn column = field.getAnnotation(KijiColumn.class);
+        Preconditions.checkState(column != null);
+
+        if (column.qualifier().isEmpty()) {
+          // TODO: Must be a map-type family!
+          // TODO: Field type must be a map of qualifier -> values/timeseries.
+
+          LOG.debug("Requesting family '{}' for field '{}'.", column.family(), field);
+          builder.addColumns(ColumnsDef.create()
+              .withMaxVersions(column.maxVersions())
+              .addFamily(column.family()));
+        } else {
+          LOG.debug("Requesting column '{}:{}' for field '{}'.",
+              column.family(), column.qualifier(), field);
+          builder.addColumns(ColumnsDef.create()
+              .withMaxVersions(column.maxVersions())
+              .add(column.family(), column.qualifier()));
+        }
+      }
+    }
+  }
+
 }
