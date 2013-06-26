691fc320bca7cd03ae127aadf471ab810fed96f6
Christophe Taton <taton@wibidata.com>
2013-06-26 11:05:38 -0700

    Test sample class

--- src/test/java/org/kiji/ohm/TestSimpleMapping.java
+++ src/test/java/org/kiji/ohm/TestSimpleMapping.java
@@ -1,11 +1,16 @@
 package org.kiji.ohm;
 
+import org.apache.hadoop.hbase.HConstants;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.kiji.ohm.annotations.EntityIdField;
+import org.kiji.ohm.annotations.KijiColumn;
+import org.kiji.ohm.annotations.KijiEntity;
 import org.kiji.schema.Kiji;
 import org.kiji.schema.KijiClientTest;
 import org.kiji.schema.KijiTable;
@@ -18,6 +23,8 @@ public class TestSimpleMapping extends KijiClientTest {
   private static final String USER_TABLE_LAYOUT = "org/kiji/ohm/user_table.json";
 
   private KijiTable mTable;
+
+  // -----------------------------------------------------------------------------------------------
 
   @Before
   public final void setup() throws Exception {
@@ -33,7 +40,31 @@ public class TestSimpleMapping extends KijiClientTest {
     mTable = null;
   }
 
+  // -----------------------------------------------------------------------------------------------
+
   @Test
   public void testSimpleMapping() throws Exception {
+  }
+
+  // -----------------------------------------------------------------------------------------------
+
+  @KijiEntity(table="user_table")
+  public static class User {
+    @EntityIdField(component=1)
+    public String eidLogin;
+
+    @KijiColumn(family="info", qualifier="login")
+    public String login;
+
+    @KijiColumn(family="info", qualifier="full_name")
+    public String fullName;
+
+    /** User birth date, in milliseconds since Epoch. */
+    @KijiColumn(family="info", qualifier="birth_date")
+    public Long birthDate;
+
+    /** User zip code. */
+    @KijiColumn(family="info", qualifier="zip_code", maxVersions=HConstants.ALL_VERSIONS)
+    public int zipCode;
   }
 }
