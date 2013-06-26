dac491ae5a754ebe69bbbdb9393ccbccd42d83c1
Christophe Taton <taton@wibidata.com>
2013-06-26 10:37:48 -0700

    Test case

---
+++ src/test/java/org/kiji/ohm/TestSimpleMapping.java
@@ -0,0 +1,25 @@
+package org.kiji.ohm;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.kiji.schema.KijiClientTest;
+import org.kiji.schema.KijiTable;
+
+public class TestSimpleMapping extends KijiClientTest {
+
+  private KijiTable mTable;
+
+  @Before
+  public final void setup() throws Exception {
+  }
+
+  @After
+  public final void teardown() throws Exception {
+  }
+
+  @Test
+  public void testSimpleMapping() throws Exception {
+  }
+}
